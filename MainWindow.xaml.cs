50b43092115abdb2b68593527dad2cf250a0d317
tgalvin001 <tgalvin001@gmail.com>
2019-08-30 18:14:25 -0500

    Included Entity Framework and ADO.NET. Interface can Read and Create.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,18 +27,80 @@ namespace WPFBasic
 
         private void ApplyMessage_Click(object sender, RoutedEventArgs e)
         {
-            if(GCCLNumber.Text.Length < 1)
-            {
-                MessageBox.Show("Descirption is empty");
-                
+            if (GCCLNumber.Text.Length < 1)
+                MessageBox.Show("GCCL number missing");
+            else
+            {                
+                using (GCCL_Part_DatabaseEntities context = new GCCL_Part_DatabaseEntities())
+                {
+                    try
+                    {
+                        int GCCLPartNumber = Int32.Parse(GCCLNumber.Text);
+                        GCCL gccl = new GCCL
+                        {
+                            GCCL1 = GCCLPartNumber,
+                            Part_Number = PartNumberTextField.Text,
+                            Manufacturer = ManufacutrerTextField.Text,
+                            Description = DesciptionTextFiled.Text
+                        };
+                        context.GCCLs.Add(gccl);
+                        context.SaveChanges();
+                        //Messagebox has no overload that formats ouput string
+                        MessageBox.Show(String.Format("GCCL item {0} Added", GCCLPartNumber));
+                        GCCLNumber.Clear();
+                        PartNumberTextField.Clear();
+                        ManufacutrerTextField.Clear();
+                        DesciptionTextFiled.Clear();
+                    }
+                    catch (FormatException)
+                    {
+                        MessageBox.Show("GCCL not a number");
+                        GCCLNumber.Clear();
+                    }                   
+                }
+
             }
-            else         
-            MessageBox.Show(GCCLNumber.Text);
         }
 
         private void ResetFields_Click(object sender, RoutedEventArgs e)
         {
             GCCLNumber.Clear();
+            PartNumberTextField.Clear();
+            ManufacutrerTextField.Clear();
+            DesciptionTextFiled.Clear();
+        }
+
+        private void SelectClicked(object sender, RoutedEventArgs e)
+        {         
+             using (GCCL_Part_DatabaseEntities context = new GCCL_Part_DatabaseEntities())
+             {
+                    //Build dynamic query
+                    IQueryable<GCCL> query = context.Set<GCCL>();
+                    
+                    if (GCCLNumber.Text != string.Empty)
+                    {
+                        int GCCLPartNumber = Int32.Parse(GCCLNumber.Text);
+                        query = query.Where(s => s.GCCL1 == GCCLPartNumber);
+                    }
+                    if(PartNumberTextField.Text != string.Empty)
+                    {
+                        query = query.Where(s => s.Part_Number == PartNumberTextField.Text);
+                    }
+                    if(ManufacutrerTextField.Text != string.Empty)
+                    {
+                        query = query.Where(s => s.Manufacturer == ManufacutrerTextField.Text);
+                    }
+                    if(DesciptionTextFiled.Text != string.Empty)
+                    {
+                        query = query.Where(s => s.Description == DesciptionTextFiled.Text);
+                    }
+                    var queryResult = query.ToList<GCCL>();
+
+                    foreach(GCCL sl in queryResult)
+                    {
+                        MessageBox.Show(sl.Description);
+                    }                                      
+             }           
         }
     }
 }
